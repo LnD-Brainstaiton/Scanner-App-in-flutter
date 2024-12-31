35fcf50201ecfb201d188f832d157c16f36e048c
Mahamud Siddiquee <mahamud.siddiquee@brainstation-23.com>
2024-12-31 15:05:11 +0600

    feat: callback path for png generated file

--- scan_23/lib/app/modules/home/views/home_view.dart
+++ scan_23/lib/app/modules/home/views/home_view.dart
@@ -1,60 +1,37 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import '/app/core/base/base_view.dart';
-import '../../../core/values/app_values.dart';
-import '../../../core/widget/paging_view.dart';
+import '../../../core/base/base_view.dart';
 import '../controllers/home_controller.dart';
-import 'package:flutter_doc_scanner/flutter_doc_scanner.dart';
-import 'package:path_provider/path_provider.dart';
 
 class HomeView extends BaseView<HomeController> {
-  var scannedFilePath = ''.obs;
-
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return null;
   }
-  Future<void> scanDocument() async {
-    dynamic scannedDocuments;
-    try {
-      scannedDocuments = await FlutterDocScanner().getScanDocuments() ??
-          'Unknown platform documents';
-    } on PlatformException {
-      scannedDocuments = 'Failed to get scanned documents.';
-    }
-    if (scannedDocuments != null && scannedDocuments is String) {
-      await saveDocument(scannedDocuments);
-    }
-  }
-
-  Future<void> saveDocument(String scannedDocumentPath) async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final savedFilePath = '${directory.path}/scanned_document.png';
-
-      final File scannedFile = File(scannedDocumentPath);
-      await scannedFile.copy(savedFilePath);
-
-      scannedFilePath.value = savedFilePath;
-      print('Document saved at: $savedFilePath');
-    } catch (e) {
-      print('Error saving document: $e');
-    }
-  }
 
   @override
   Widget body(BuildContext context) {
+    final HomeController controller = Get.find<HomeController>();
+
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            scanDocument();
-            print('Button Pressed!');
-          },
-          child: Text('Scan'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(() {
+              return controller.scannedDocuments.value != null
+                  ? Text(controller.scannedDocuments.value.toString())
+                  : const Text("No Documents Scanned");
+            }),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                controller.scanDocument();
+                print('Scan Button Pressed!');
+              },
+              child: const Text('Scan'),
+            ),
+          ],
         ),
       ),
     );
